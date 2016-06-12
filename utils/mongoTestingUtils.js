0918f77a5b4090e7d010b3c302c8b287bff199f6
m4nuC <m4nu@hotmail.fr>
2016-06-12 15:40:43 +0800

    Add testing Utils

---
+++ utils/mongoTestingUtils.js
@@ -0,0 +1,49 @@
+import {mongoInit, mongoClose} from '../bw_commons/services/mongoDBConnector';
+import migrate from 'migrate';
+const set = migrate.load('migrations/.migrate-tests', 'migrations');
+
+export function prepareMongo(done) {
+  set.up(function (err) {
+    if (err) throw done(err);
+    done();
+  });
+}
+
+export function cleanupMongo(done) {
+  set.down(function (err) {
+    if (err) throw done(err);
+  });
+  done();
+}
+
+/**
+ * Prepare a mongoDB collection for test.
+ *
+ * Typically this is used in the beforeEach hook
+ *
+ * Example use:
+ * beforeEach(function(done) {
+ *   setAndPrepareCollections(['todos'], async () =>{
+ *     // After collection is emptied, Seed new data
+ *     getMongoPool().collection('todos').insertOne(todoStub).then(res => {
+ *       done()
+ *     });
+ *   });
+ * });
+ *
+ *
+ * @param  {Array}   collections: The name of the collections to be cleared
+ * @param  {Function} cb: Callback function that will be called after the DB has been setup
+ * @return {Promise}
+ */
+export function setAndPrepareCollections(collections, cb) {
+  mongoInit().then((db) => {
+    const deletes = collections.map((collection) => {
+      return db.collection(collection).remove({});
+    });
+    return Promise.all(deletes).then(function(err, res) {
+      db.collection('users').insert(seedUserData);
+      cb()
+    });
+  });
+}
